1325fe96bec652f6dc77a9bf3691d48f2d9a1e7e
Levi Crain <levi.crain@lacuna.ai>
2019-10-23 14:00:07 -0400

    Revert "Removing chai because `should` does  promise testing"
    This reverts commit d6476876897ee93a9b45fe0e1781d642486e4d2e.

--- packages/mds-identity/tests/identity-provider.spec.ts
+++ packages/mds-identity/tests/identity-provider.spec.ts
@@ -1,8 +1,11 @@
 import sinon from 'sinon'
 import axios from 'axios'
-/* eslint-disable-next-line @typescript-eslint/no-unused-vars */
-import should from 'should'
+import chai from 'chai'
+import chaiAsPromised from 'chai-as-promised'
 import IdentityProvider from '../identity-provider'
+
+const { expect } = chai
+chai.use(chaiAsPromised)
 
 describe('Identity Provider', () => {
   afterEach(() => {
@@ -21,30 +24,24 @@ describe('Identity Provider', () => {
     const mockTokenData = { access_token: '111', id_token: '222', expires_in: 333, scope: 'test scope' }
     sinon.stub(axios, 'post').resolves({ data: mockTokenData })
 
-    await IdentityProvider.CodeAuthenticate(codeAuthenticateParameters).should.eventually.eql(mockTokenData)
+    await expect(IdentityProvider.CodeAuthenticate(codeAuthenticateParameters)).to.eventually.eql(mockTokenData)
   })
 
   it('Rejects when idp returns 403', async () => {
     sinon.stub(axios, 'post').rejects({ response: { status: 403, statusText: 'unauthorized', data: 'unauthorized' } })
 
-    await IdentityProvider.CodeAuthenticate(codeAuthenticateParameters).should.be.rejected
+    await expect(IdentityProvider.CodeAuthenticate(codeAuthenticateParameters)).to.be.rejected
   })
 
   it('Rejects when idp returns 401', async () => {
     sinon.stub(axios, 'post').rejects({ response: { status: 401, statusText: 'forbidden', data: 'forbidden' } })
 
-    await IdentityProvider.CodeAuthenticate(codeAuthenticateParameters).should.be.rejected
-  })
-
-  it('Rejects when idp returns 500', async () => {
-    sinon.stub(axios, 'post').rejects({ response: { status: 500, statusText: 'server error', data: 'server_error' } })
-
-    await IdentityProvider.CodeAuthenticate(codeAuthenticateParameters).should.be.rejected
+    await expect(IdentityProvider.CodeAuthenticate(codeAuthenticateParameters)).to.be.rejected
   })
 
   it('Rejects when idp returns 200 but unexpected results', async () => {
     sinon.stub(axios, 'post').rejects({ response: { wtf: 123 } })
 
-    await IdentityProvider.CodeAuthenticate(codeAuthenticateParameters).should.be.rejected
+    await expect(IdentityProvider.CodeAuthenticate(codeAuthenticateParameters)).to.be.rejected
   })
 })
