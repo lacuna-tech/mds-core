d596293c2512ead2cd064b9248854dbb8567f44e
Neil Goldader <neilgoldader@gmail.com>
2019-11-05 16:57:14 -0800

    WIP

--- packages/mds-metrics/request-handlers.ts
+++ packages/mds-metrics/request-handlers.ts
@@ -2,14 +2,37 @@ import log from '@mds-core/mds-logger'
 
 import db from '@mds-core/mds-db'
 import { inc } from '@mds-core/mds-utils'
-import { VEHICLE_TYPES } from 'packages/mds-types'
-import { MetricsApiRequest, MetricsApiResponse, instantiateEventSnapshotResponse } from './types'
+import { VEHICLE_TYPES, EVENT_STATUS_MAP } from '@mds-core/mds-types'
+import {
+  MetricsApiRequest,
+  MetricsApiResponse,
+  instantiateEventSnapshotResponse,
+  instantiateStateSnapshotResponse
+} from './types'
+
 // import db from '@mds-core/mds-db'
 
 export async function getStateSnapshot(req: MetricsApiRequest, res: MetricsApiResponse) {
   const { params } = req
   log.info(params)
-  res.status(200).send('Tada')
+
+  const { events } = await db.readEvents(params)
+  const device_ids = (await db.readDeviceIds(params.provider_id)).map(device => device.device_id)
+  const devices = await db.readDeviceList(device_ids)
+  const statusCounts = events.reduce((acc, event) => {
+    const { event_type, device_id } = event
+    const status = EVENT_STATUS_MAP[event_type]
+
+    const { type } = devices.find(d => {
+      return d.device_id === device_id
+    }) || { type: VEHICLE_TYPES.scooter }
+
+    const incrementedSubAcc = { [type]: inc(acc[type], status) }
+
+    return { ...acc, incrementedSubAcc }
+  }, instantiateStateSnapshotResponse(0))
+
+  res.status(200).send(statusCounts)
 }
 
 export async function getEventSnapshot(req: MetricsApiRequest, res: MetricsApiResponse) {
--- packages/mds-metrics/types.ts
+++ packages/mds-metrics/types.ts
@@ -1,16 +1,20 @@
 import { ApiRequest, ApiResponse } from '@mds-core/mds-api-server'
 import { VEHICLE_STATUS, VEHICLE_EVENT, VEHICLE_TYPES } from '@mds-core/mds-types'
-import { VEHICLE_TYPE, VEHICLE_EVENTS } from 'packages/mds-types/dist'
+import { VEHICLE_TYPE, VEHICLE_EVENTS, VEHICLE_STATUSES } from 'packages/mds-types/dist'
 
 export type MetricsApiRequest = ApiRequest
 export type MetricsApiResponse = ApiResponse
 
 export type StateSnapshotResponse = {
-  vehicle_type: { [T in VEHICLE_TYPE]: { [S in VEHICLE_STATUS]: number } }
+  [T in VEHICLE_TYPE]: { [S in VEHICLE_STATUS]: number }
 }
 
 export type EventSnapshotResponse = {
   [T in VEHICLE_TYPE]: { [S in VEHICLE_EVENT]: number }
+}
+
+export type CountsResponse = {
+  [T in VEHICLE_TYPE]: number
 }
 
 export function instantiateEventSnapshotResponse(value: number) {
@@ -25,6 +29,14 @@ export function instantiateEventSnapshotResponse(value: number) {
   }, {}) as EventSnapshotResponse
 }
 
-export type CountsResponse = {
-  vehicle_type: { [T in VEHICLE_TYPE]: number }
+export function instantiateStateSnapshotResponse(value: number) {
+  return Object.keys(VEHICLE_TYPES).reduce((acc, vehicle_type) => {
+    const typeRow = {
+      [vehicle_type]: Object.keys(VEHICLE_STATUSES).reduce((acc2, event_type) => {
+        const eventRow = { [event_type]: value }
+        return { ...acc2, eventRow }
+      }, {})
+    }
+    return { ...acc, typeRow }
+  }, {}) as StateSnapshotResponse
 }
