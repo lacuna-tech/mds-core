19a8bf0ee04814452f6c14e438cce1a0c8f36ba8
Neil Goldader <neilgoldader@gmail.com>
2019-11-07 16:25:11 -0800

    Some cleanup

--- packages/mds-metrics/request-handlers.ts
+++ packages/mds-metrics/request-handlers.ts
@@ -3,13 +3,13 @@ import log from '@mds-core/mds-logger'
 import db from '@mds-core/mds-db'
 import { inc, RuntimeError, yesterday } from '@mds-core/mds-utils'
 import { EVENT_STATUS_MAP } from '@mds-core/mds-types'
+import { now } from 'moment'
 import {
   MetricsApiRequest,
   MetricsApiResponse,
   instantiateEventSnapshotResponse,
   instantiateStateSnapshotResponse
 } from './types'
-import { now } from 'moment'
 
 // import db from '@mds-core/mds-db'
 
@@ -20,6 +20,7 @@ export async function getStateSnapshot(req: MetricsApiRequest, res: MetricsApiRe
   const { events } = await db.readEvents(params)
   const device_ids = (await db.readDeviceIds(params.provider_id)).map(device => device.device_id)
   const devices = await db.readDeviceList(device_ids)
+
   const statusCounts = events.reduce((acc, event) => {
     const { event_type, device_id } = event
     const status = EVENT_STATUS_MAP[event_type]
@@ -47,6 +48,7 @@ export async function getEventSnapshot(req: MetricsApiRequest, res: MetricsApiRe
   const { events } = await db.readEvents(params)
   const device_ids = (await db.readDeviceIds(params.provider_id)).map(device => device.device_id)
   const devices = await db.readDeviceList(device_ids)
+
   const eventCounts = events.reduce((acc, event) => {
     const { event_type, device_id } = event
 
@@ -69,19 +71,21 @@ export async function getEventSnapshot(req: MetricsApiRequest, res: MetricsApiRe
 export async function getTelemetryCounts(req: MetricsApiRequest, res: MetricsApiResponse) {
   const { params } = req
 
-  const {start_time = now(), end_time = yesterday(), bin = 3600000} = params
+  const { start_time = now(), end_time = yesterday(), bin = 3600000 } = params
 
   const slices = []
 
   for (const time: number = start_time; time < end_time; time + bin) {
     const next_time = time + bin
-    slices.push({start: time, end: next_time})
+    slices.push({ start: time, end: next_time })
   }
 
-  const telemetryCounts = Promise.all(slices.map(slice => {
-    const {start, end} = slice
-    return db.getTelemetryCountsPerProviderSince(start, end)
-  }))
+  const telemetryCounts = Promise.all(
+    slices.map(slice => {
+      const { start, end } = slice
+      return db.getTelemetryCountsPerProviderSince(start, end)
+    })
+  )
 
   res.status(200).send(telemetryCounts)
 }
@@ -89,19 +93,21 @@ export async function getTelemetryCounts(req: MetricsApiRequest, res: MetricsApi
 export async function getEventCounts(req: MetricsApiRequest, res: MetricsApiResponse) {
   const { params } = req
 
-  const {start_time = now(), end_time = yesterday(), bin = 3600000} = params
+  const { start_time = now(), end_time = yesterday(), bin = 3600000 } = params
 
   const slices = []
 
   for (const time: number = start_time; time < end_time; time + bin) {
     const next_time = time + bin
-    slices.push({start: time, end: next_time})
+    slices.push({ start: time, end: next_time })
   }
 
-  const eventCounts = Promise.all(slices.map(slice => {
-    const {start, end} = slice
-    return db.getEventCountsPerProviderSince(start, end)
-  }))
+  const eventCounts = Promise.all(
+    slices.map(slice => {
+      const { start, end } = slice
+      return db.getEventCountsPerProviderSince(start, end)
+    })
+  )
 
   res.status(200).send(eventCounts)
 }
