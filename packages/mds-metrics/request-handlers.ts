35a904a7b493e96c919a31a74972c4762840eed7
Neil Goldader <neilgoldader@gmail.com>
2019-11-11 10:27:23 -0800

    Review nits

--- packages/mds-metrics/request-handlers.ts
+++ packages/mds-metrics/request-handlers.ts
@@ -1,162 +1,171 @@
-import log from '@mds-core/mds-logger'
-
 import db from '@mds-core/mds-db'
-import { inc, RuntimeError, yesterday } from '@mds-core/mds-utils'
+import { inc, RuntimeError, yesterday, ServerError } from '@mds-core/mds-utils'
 import { EVENT_STATUS_MAP } from '@mds-core/mds-types'
 import { now } from 'moment'
+import log from '@mds-core/mds-logger'
 import {
   MetricsApiRequest,
-  MetricsApiResponse,
   instantiateEventSnapshotResponse,
-  instantiateStateSnapshotResponse
+  instantiateStateSnapshotResponse,
+  GetStateSnapshotResponse,
+  GetEventsSnapshotResponse,
+  GetTelemetryCountsResponse,
+  GetEventCountsResponse,
+  TelemetryCountsResponse,
+  EventSnapshotResponse,
+  StateSnapshotResponse
 } from './types'
+import { getTimeBins } from './utils'
 
-export async function getStateSnapshot(req: MetricsApiRequest, res: MetricsApiResponse) {
-  const { params } = req
-  log.info(params)
+export async function getStateSnapshot(req: MetricsApiRequest, res: GetStateSnapshotResponse) {
+  const { body } = req
 
-  const { start_time = yesterday(), end_time = now(), bin = 3600000 } = params
+  const { start_time = yesterday(), end_time = now(), bin_size = 3600000, provider_id } = body
 
-  const interval = end_time - start_time
+  const slices = getTimeBins({ start_time, end_time, bin_size })
 
-  const bins = new Array(Math.floor(interval / bin))
+  try {
+    const device_ids = (await db.readDeviceIds(provider_id)).map(device => device.device_id)
+    const devices = await db.readDeviceList(device_ids)
 
-  const slices = bins.map((_, idx) => ({
-    start: start_time + idx * bin,
-    end: start_time + (idx + 1) * bin
-  }))
+    const eventsBySlice = await Promise.all(
+      slices.map(slice => {
+        const { end } = slice
+        return db.readHistoricalEvents({ end_date: end })
+      })
+    )
 
-  const device_ids = (await db.readDeviceIds(params.provider_id)).map(device => device.device_id)
-  const devices = await db.readDeviceList(device_ids)
+    const result = eventsBySlice
+      .map(events => {
+        if (events) {
+          const statusCounts = events.reduce((acc, event) => {
+            const { event_type, device_id } = event
+            const status = EVENT_STATUS_MAP[event_type]
 
-  const eventsBySlice = await Promise.all(
-    slices.map(slice => {
-      const { end } = slice
-      return db.readHistoricalEvents({ end_date: end })
-    })
-  )
+            const { type } = devices.find(d => {
+              return d.device_id === device_id
+            }) || { type: undefined }
 
-  const result = eventsBySlice.map(events => {
-    if (events) {
-      const statusCounts = events.reduce((acc, event) => {
-        const { event_type, device_id } = event
-        const status = EVENT_STATUS_MAP[event_type]
+            if (type === undefined) {
+              throw new RuntimeError(`Could not find corresponding device ${device_id} for event ${event}!`)
+            }
 
-        const { type } = devices.find(d => {
-          return d.device_id === device_id
-        }) || { type: undefined }
+            const incrementedSubAcc = { [type]: inc(acc[type], status) }
 
-        if (type === undefined) {
-          throw new RuntimeError(`Could not find corresponding device ${device_id} for event ${event}!`)
+            return { ...acc, incrementedSubAcc }
+          }, instantiateStateSnapshotResponse(0))
+
+          return statusCounts
         }
+      })
+      .filter((e): e is StateSnapshotResponse => e !== undefined)
 
-        const incrementedSubAcc = { [type]: inc(acc[type], status) }
-
-        return { ...acc, incrementedSubAcc }
-      }, instantiateStateSnapshotResponse(0))
-
-      return statusCounts
-    }
-  })
-
-  res.status(200).send(result)
+    res.status(200).send(result)
+  } catch (error) {
+    await log.error(error)
+    res.status(500).send(new ServerError())
+  }
 }
 
-export async function getEventSnapshot(req: MetricsApiRequest, res: MetricsApiResponse) {
-  const { params } = req
-  log.info(params)
+export async function getEventSnapshot(req: MetricsApiRequest, res: GetEventsSnapshotResponse) {
+  const { body } = req
 
-  const { start_time = yesterday(), end_time = now(), bin = 3600000 } = params
+  const { start_time = yesterday(), end_time = now(), bin_size = 3600000, provider_id } = body
 
-  const interval = end_time - start_time
+  const slices = getTimeBins({ start_time, end_time, bin_size })
 
-  const bins = new Array(Math.floor(interval / bin))
+  try {
+    const device_ids = (await db.readDeviceIds(provider_id)).map(device => device.device_id)
+    const devices = await db.readDeviceList(device_ids)
 
-  const slices = bins.map((_, idx) => ({ start: start_time + idx * bin, end: start_time + (idx + 1) * bin }))
+    const eventsBySlice = await Promise.all(
+      slices.map(slice => {
+        const { end } = slice
+        return db.readHistoricalEvents({ end_date: end })
+      })
+    )
 
-  const device_ids = (await db.readDeviceIds(params.provider_id)).map(device => device.device_id)
-  const devices = await db.readDeviceList(device_ids)
+    const result = eventsBySlice
+      .map(events => {
+        if (events) {
+          const eventCounts = events.reduce((acc, event) => {
+            const { event_type, device_id } = event
 
-  const eventsBySlice = await Promise.all(
-    slices.map(slice => {
-      const { end } = slice
-      return db.readHistoricalEvents({ end_date: end })
-    })
-  )
+            const { type } = devices.find(d => {
+              return d.device_id === device_id
+            }) || { type: undefined }
 
-  const result = eventsBySlice.map(events => {
-    if (events) {
-      const eventCounts = events.reduce((acc, event) => {
-        const { event_type, device_id } = event
+            if (type === undefined) {
+              throw new RuntimeError(`Could not find corresponding device ${device_id} for event ${event}!`)
+            }
 
-        const { type } = devices.find(d => {
-          return d.device_id === device_id
-        }) || { type: undefined }
+            const incrementedSubAcc = { [type]: inc(acc[type], event_type) }
 
-        if (type === undefined) {
-          throw new RuntimeError(`Could not find corresponding device ${device_id} for event ${event}!`)
+            return { ...acc, incrementedSubAcc }
+          }, instantiateEventSnapshotResponse(0))
+
+          return eventCounts
         }
+      })
+      .filter((e): e is EventSnapshotResponse => e !== undefined)
 
-        const incrementedSubAcc = { [type]: inc(acc[type], event_type) }
-
-        return { ...acc, incrementedSubAcc }
-      }, instantiateEventSnapshotResponse(0))
-
-      return eventCounts
-    }
-  })
-
-  res.status(200).send(result)
+    res.status(200).send(result)
+  } catch (error) {
+    await log.error(error)
+    res.status(500).send(new ServerError())
+  }
 }
 
-export async function getTelemetryCounts(req: MetricsApiRequest, res: MetricsApiResponse) {
-  const { params } = req
+export async function getTelemetryCounts(req: MetricsApiRequest, res: GetTelemetryCountsResponse) {
+  const { body } = req
 
-  const { start_time = yesterday(), end_time = now(), bin = 3600000 } = params
+  const { start_time = yesterday(), end_time = now(), bin_size = 3600000 } = body
 
-  const interval = end_time - start_time
+  const slices = getTimeBins({ start_time, end_time, bin_size })
 
-  const bins = new Array(Math.floor(interval / bin))
+  try {
+    const telemetryCounts = await Promise.all(
+      slices.map(slice => {
+        const { start, end } = slice
+        return db.getTelemetryCountsPerProviderSince(start, end)
+      })
+    )
 
-  const slices = bins.map((_, idx) => ({ start: start_time + idx * bin, end: start_time + (idx + 1) * bin }))
-
-  const telemetryCounts = await Promise.all(
-    slices.map(slice => {
-      const { start, end } = slice
-      return db.getTelemetryCountsPerProviderSince(start, end)
+    const telemetryCountsWithTimeSlices: TelemetryCountsResponse[] = telemetryCounts.map((telemetryCount, idx) => {
+      const slice = slices[idx]
+      return { telemetryCount, slice }
     })
-  )
 
-  const telemetryCountsWithTimeSlices = telemetryCounts.map((telemetryCount, idx) => {
-    const slice = slices[idx]
-    return { telemetryCount, slice }
-  })
-
-  res.status(200).send(telemetryCountsWithTimeSlices)
+    res.status(200).send(telemetryCountsWithTimeSlices)
+  } catch (error) {
+    await log.error(error)
+    res.status(500).send(new ServerError())
+  }
 }
 
-export async function getEventCounts(req: MetricsApiRequest, res: MetricsApiResponse) {
-  const { params } = req
+export async function getEventCounts(req: MetricsApiRequest, res: GetEventCountsResponse) {
+  const { body } = req
 
-  const { start_time = yesterday(), end_time = now(), bin = 3600000 } = params
+  const { start_time = yesterday(), end_time = now(), bin_size = 3600000 } = body
 
-  const interval = end_time - start_time
+  const slices = getTimeBins({ start_time, end_time, bin_size })
 
-  const bins = new Array(Math.floor(interval / bin))
+  try {
+    const eventCounts = await Promise.all(
+      slices.map(slice => {
+        const { start, end } = slice
+        return db.getEventCountsPerProviderSince(start, end)
+      })
+    )
 
-  const slices = bins.map((_, idx) => ({ start: start_time + idx * bin, end: start_time + (idx + 1) * bin }))
-
-  const eventCounts = await Promise.all(
-    slices.map(slice => {
-      const { start, end } = slice
-      return db.getEventCountsPerProviderSince(start, end)
+    const eventCountsWithTimeSlice = eventCounts.map((eventCount, idx) => {
+      const slice = slices[idx]
+      return { eventCount, slice }
     })
-  )
 
-  const eventCountsWithTimeSlice = eventCounts.map((eventCount, idx) => {
-    const slice = slices[idx]
-    return { eventCount, slice }
-  })
-
-  res.status(200).send(eventCountsWithTimeSlice)
+    res.status(200).send(eventCountsWithTimeSlice)
+  } catch (error) {
+    await log.error(error)
+    res.status(500).send(new ServerError(error))
+  }
 }
--- packages/mds-metrics/types.ts
+++ packages/mds-metrics/types.ts
@@ -5,11 +5,10 @@ import {
   VEHICLE_TYPES,
   VEHICLE_TYPE,
   VEHICLE_EVENTS,
-  VEHICLE_STATUSES
+  VEHICLE_STATUSES,
+  Timestamp,
+  UUID
 } from '@mds-core/mds-types'
-
-export type MetricsApiRequest = ApiRequest
-export type MetricsApiResponse = ApiResponse
 
 export type StateSnapshotResponse = {
   [T in VEHICLE_TYPE]: { [S in VEHICLE_STATUS]: number }
@@ -19,30 +18,70 @@ export type EventSnapshotResponse = {
   [T in VEHICLE_TYPE]: { [S in VEHICLE_EVENT]: number }
 }
 
-export type CountsResponse = {
-  [T in VEHICLE_TYPE]: number
+export type TelemetryCountsResponse = {
+  telemetryCount: {
+    count: number
+    slacount: number
+    provider_id: UUID
+  }[]
+  slice: {
+    start: number
+    end: number
+  }
 }
 
-export function instantiateEventSnapshotResponse(value: number) {
-  return Object.keys(VEHICLE_TYPES).reduce((acc, vehicle_type) => {
-    const typeRow = {
+export type EventCountsResponse = {
+  eventCount: {
+    count: number
+    slacount: number
+    event_type: string
+    provider_id: UUID
+  }[]
+  slice: {
+    start: number
+    end: number
+  }
+}
+
+export type GetTimeBinsParams = {
+  start_time: Timestamp
+  end_time: Timestamp
+  bin_size: Timestamp
+}
+export interface MetricsApiRequest extends ApiRequest {
+  query: Partial<
+    {
+      [P in 'start_time' | 'end_time' | 'bin']: string
+    }
+  >
+}
+
+export type MetricsApiResponse<T> = ApiResponse<T | Error>
+export type GetStateSnapshotResponse = MetricsApiResponse<StateSnapshotResponse[]>
+export type GetEventsSnapshotResponse = MetricsApiResponse<EventSnapshotResponse[]>
+export type GetTelemetryCountsResponse = MetricsApiResponse<TelemetryCountsResponse[]>
+export type GetEventCountsResponse = MetricsApiResponse<EventCountsResponse[]>
+
+export const instantiateEventSnapshotResponse = (value: number) =>
+  Object.keys(VEHICLE_TYPES).reduce(
+    (acc, vehicle_type) => ({
+      ...acc,
       [vehicle_type]: Object.keys(VEHICLE_EVENTS).reduce((acc2, event_type) => {
         const eventRow = { [event_type]: value }
         return { ...acc2, eventRow }
       }, {})
-    }
-    return { ...acc, typeRow }
-  }, {}) as EventSnapshotResponse
-}
+    }),
+    {}
+  ) as EventSnapshotResponse
 
-export function instantiateStateSnapshotResponse(value: number) {
-  return Object.keys(VEHICLE_TYPES).reduce((acc, vehicle_type) => {
-    const typeRow = {
+export const instantiateStateSnapshotResponse = (value: number) =>
+  Object.keys(VEHICLE_TYPES).reduce(
+    (acc, vehicle_type) => ({
+      ...acc,
       [vehicle_type]: Object.keys(VEHICLE_STATUSES).reduce((acc2, event_type) => {
         const eventRow = { [event_type]: value }
         return { ...acc2, eventRow }
       }, {})
-    }
-    return { ...acc, typeRow }
-  }, {}) as StateSnapshotResponse
-}
+    }),
+    {}
+  ) as StateSnapshotResponse
