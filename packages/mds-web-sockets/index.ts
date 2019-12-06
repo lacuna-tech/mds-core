4192474a60b127dbce274169027c58f50a77b9ed
Neil Goldader <neilgoldader@gmail.com>
2019-12-06 14:01:15 -0500

    WIP

--- packages/mds-web-sockets/index.ts
+++ packages/mds-web-sockets/index.ts
@@ -2,6 +2,7 @@ import log from '@mds-core/mds-logger'
 import WebSocket from 'ws'
 import { Telemetry, VehicleEvent } from '@mds-core/mds-types'
 import { ApiServer } from '@mds-core/mds-api-server'
+import { Clients } from './clients'
 
 const {
   env: { npm_package_name, PORT = 4001 }
@@ -11,27 +12,28 @@ const server = ApiServer(app => app).listen(PORT, () => log.info(`${npm_package_
 
 const wss = new WebSocket.Server({ server })
 
-let cachedWs: WebSocket | null = null
+const clients = new Clients()
 
 wss.on('connection', (ws: WebSocket) => {
-  cachedWs = ws
+  ws.on('message', (data: string) => {
+    clients.saveClient(data.split(','), ws)
+  })
 })
 
-function getClient() {
-  if (!cachedWs) {
-    wss.on('connection', (ws: WebSocket) => {
-      cachedWs = ws
-    })
-  }
-  return cachedWs as WebSocket
+// TODO: Subscribe to telemetry and event streams from KNE, and call writeTelemetry/writeEvent
+
+function pushToClients(entity: string, message: string) {
+  clients.map(client => {
+    // looks good
+    // if client has subscribed lalalala
+    client.emit(entity, message)
+  })
 }
 
-export function writeTelemetry(telemetry: Telemetry) {
-  const ws = getClient()
-  ws.emit('TELEMETRIES', JSON.stringify(telemetry))
+function writeTelemetry(telemetry: Telemetry) {
+  pushToClients('TELEMETRIES', JSON.stringify(telemetry))
 }
 
-export function writeEvent(event: VehicleEvent) {
-  const ws = getClient()
-  ws.emit('EVENTS', JSON.stringify(event))
+function writeEvent(event: VehicleEvent) {
+  pushToClients('EVENTS', JSON.stringify(event))
 }
