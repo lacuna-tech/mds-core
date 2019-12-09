bb6d7971e1e3593d6e5fb5d214100d5a84386aca
drtyh20 <michael@ellis-and-associates.com>
2019-12-09 10:09:03 -0500

    Handle parse errors from cron source events

--- packages/mds-event-server/index.ts
+++ packages/mds-event-server/index.ts
@@ -2,7 +2,7 @@ import express from 'express'
 import logger from '@mds-core/mds-logger'
 import { pathsFor, ServerError } from '@mds-core/mds-utils'
 import { AboutRequestHandler, HealthRequestHandler, JsonBodyParserMiddleware } from '@mds-core/mds-api-server'
-import Cloudevent, { BinaryHTTPReceiver } from 'cloudevents-sdk/v1'
+import Cloudevent, { event as cloudevent, BinaryHTTPReceiver } from 'cloudevents-sdk/v1'
 
 export const EventServer = <TData, TResult>(
   handler: (type: string, data: TData, event: Cloudevent) => Promise<TResult>,
@@ -12,6 +12,21 @@ export const EventServer = <TData, TResult>(
   server.disable('x-powered-by')
 
   const receiver = new BinaryHTTPReceiver()
+
+  const parseCloudEvent = (req: express.Request): Cloudevent => {
+    try {
+      return receiver.parse(req.body, req.headers)
+    } catch (error) {
+      const [source, type] = [req.header('ce-source'), req.header('ce-type')]
+      if (source && type) {
+        return cloudevent()
+          .source(source)
+          .type(type)
+          .data(req.body)
+      }
+      throw error
+    }
+  }
 
   // Middleware
   server.use(JsonBodyParserMiddleware({ limit: '1mb' }))
@@ -23,9 +38,9 @@ export const EventServer = <TData, TResult>(
 
   server.post('/', async (req, res) => {
     try {
-      const cloudevent = receiver.parse(req.body, req.headers)
-      await logger.info('PARSE Cloud Event', 'BODY:', req.body, 'HEADERS:', req.headers, 'EVENT:', cloudevent.format())
-      const result = await handler(cloudevent.getType(), cloudevent.getData(), cloudevent)
+      const event = parseCloudEvent(req)
+      await logger.info('PARSE Cloud Event', 'BODY:', req.body, 'HEADERS:', req.headers, 'EVENT:', event.format())
+      const result = await handler(event.getType(), event.getData(), event)
       return res.status(200).send({ result })
     } catch (error) {
       await logger.error('ERROR Cloud Event', 'BODY:', req.body, 'HEADERS:', req.headers, 'ERROR:', error)
