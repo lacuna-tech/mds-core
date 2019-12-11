ca8f13164fa503aed1c7873307f3cd2d35a0c90e
Max Johansen <macsj200@gmail.com>
2019-12-11 15:59:54 -0700

    revert unrelated changes

--- packages/mds-metrics/api.ts
+++ packages/mds-metrics/api.ts
@@ -18,14 +18,7 @@ import express from 'express'
 
 import { pathsFor } from '@mds-core/mds-utils'
 import { checkAccess } from '@mds-core/mds-api-server'
-import {
-  getStateSnapshot,
-  getEventSnapshot,
-  getTelemetryCounts,
-  getEventCounts,
-  getAll,
-  getAllStubbed
-} from './request-handlers'
+import { getStateSnapshot, getEventSnapshot, getTelemetryCounts, getEventCounts, getAll, getAllStubbed } from './request-handlers'
 
 function api(app: express.Express): express.Express {
   app.get(
