0b520a45e999d2a7dbc84552abbdea5d3b9b1563
Max Johansen <macsj200@gmail.com>
2020-01-07 13:25:42 -0700

    add expected transition values

--- packages/mds-utils/tests/utils.spec.ts
+++ packages/mds-utils/tests/utils.spec.ts
@@ -27,6 +27,7 @@ import {
   isStateTransitionValid,
   normalizeToArray
 } from '../utils'
+import { expectedTransitions } from './state-transition-expected'
 
 const Boston = { lat: 42.360081, lng: -71.058884 }
 const LosAngeles = { lat: 34.052235, lng: -118.243683 }
@@ -156,12 +157,9 @@ describe('Tests Utilities', () => {
         for (const event_type_B of events) {
           const eventA = { event_type: event_type_A } as VehicleEvent
           const eventB = { event_type: event_type_B } as VehicleEvent
-          assert.strictEqual(
-            // TODO hardcode responses
-            isStateTransitionValid(eventA, eventB) || true,
-            true,
-            `${eventA.event_type}, ${eventB.event_type}`
-          )
+          const actual = isStateTransitionValid(eventA, eventB)
+          const transitionKey = `${eventA.event_type}, ${eventB.event_type}`
+          assert.strictEqual(actual, expectedTransitions[transitionKey], transitionKey)
         }
       }
     })
