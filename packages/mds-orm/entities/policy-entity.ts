6998a3a7ff74567fd9058263cbcc1104a4982a07
jian <jane.x.huang@gmail.com>
2020-01-16 19:53:35 -0500

    Adding TypeORM policy entity

--- packages/mds-orm/entities/policy-entity.ts
+++ packages/mds-orm/entities/policy-entity.ts
@@ -1,5 +1,19 @@
-import { Entity } from 'typeorm'
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm'
 import { IdentityEntity } from './identity-entity'
+import { UUID } from '@mds-core/mds-types'
+import { FeatureCollection } from 'geojson'
+// [TABLE.policies]: [COLUMN.id, COLUMN.policy_id, COLUMN.policy_json],
 
+//  [COLUMN.policy_id]: 'uuid NOT NULL',
+//   [COLUMN.policy_json]: 'json NOT NULL',
 @Entity('policies')
-export class PolicyEntity extends IdentityEntity {}
+export class PolicyEntity extends IdentityEntity {
+  @PrimaryGeneratedColumn()
+  id: number
+
+  @Column('uuid', { primary: true })
+  policy_id: UUID
+
+  @Column('json')
+  policy_json: FeatureCollection
+}
