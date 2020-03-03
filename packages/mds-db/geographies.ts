0bfbb258bd0afa3f90e3c09cd9cf65913fb4660b
Jane Huang <jane.x.huang@gmail.com>
2020-03-02 23:49:07 -0500

    Extract geography author service from policy author.

--- packages/mds-db/geographies.ts
+++ packages/mds-db/geographies.ts
@@ -1,5 +1,5 @@
 import { Geography, GeographySummary, UUID, Recorded, GeographyMetadata } from '@mds-core/mds-types'
-import { NotFoundError } from '@mds-core/mds-utils'
+import { BadParamsError, NotFoundError, DependencyMissingError, AlreadyPublishedError } from '@mds-core/mds-utils'
 import log from '@mds-core/mds-logger'
 
 import schema from './schema'
@@ -10,33 +10,51 @@ import { getReadOnlyClient, getWriteableClient } from './client'
 import { ReadGeographiesParams, PublishGeographiesParams } from './types'
 
 export async function readSingleGeography(geography_id: UUID): Promise<Geography> {
-  try {
-    const client = await getReadOnlyClient()
+  const client = await getReadOnlyClient()
 
-    const sql = `select * from ${schema.TABLE.geographies} where geography_id = '${geography_id}'`
-    const { rows } = await client.query(sql)
+  const sql = `select * from ${schema.TABLE.geographies} where geography_id = '${geography_id}'`
+  const { rows } = await client.query(sql)
 
-    const { id, ...geography } = rows[0]
-    return geography
-  } catch (err) {
-    await log.error('readSingleGeography', err)
-    throw new NotFoundError(`could not find geography ${geography_id}`)
+  if (rows.length === 0) {
+    await log.info(`readSingleGeography failed for ${geography_id}`)
+    throw new NotFoundError(`geography of id ${geography_id} not found`)
   }
+
+  const { id, ...geography } = rows[0]
+  return geography
 }
 
 export async function readGeographies(params: Partial<ReadGeographiesParams> = {}): Promise<Geography[]> {
   try {
     const client = await getReadOnlyClient()
 
-    const { get_read_only } = { get_read_only: false, ...params }
+    const { get_published, get_unpublished, geography_ids } = {
+      get_published: false,
+      get_unpublished: false,
+      ...params
+    }
+    if (get_published && get_unpublished) {
+      throw new BadParamsError('cannot have get_unpublished and get_published both be true')
+    }
 
     let sql = `SELECT * FROM ${schema.TABLE.geographies}`
 
     const conditions = []
     const vals = new SqlVals()
 
-    if (get_read_only) {
+    if (get_published) {
       conditions.push(`publish_date IS NOT NULL`)
+    }
+
+    if (get_unpublished) {
+      conditions.push(`publish_date IS NULL`)
+    }
+
+    if (geography_ids) {
+      const SQLified_geography_ids = geography_ids.map(id => {
+        return `'${id}'`
+      })
+      conditions.push(`geography_id in (${SQLified_geography_ids.join(',')})`)
     }
 
     if (conditions.length) {
@@ -58,7 +76,7 @@ export async function readGeographies(params: Partial<ReadGeographiesParams> = {
   }
 }
 
-export async function readGeographySummaries(params?: { get_read_only?: boolean }): Promise<GeographySummary[]> {
+export async function readGeographySummaries(params?: ReadGeographiesParams): Promise<GeographySummary[]> {
   const geographies = await readGeographies(params)
   return geographies.map(geography => {
     const { geography_json, ...geographySummary } = geography
@@ -66,7 +84,7 @@ export async function readGeographySummaries(params?: { get_read_only?: boolean 
   })
 }
 
-export async function readBulkGeographyMetadata(params?: { get_read_only?: boolean }): Promise<GeographyMetadata[]> {
+export async function readBulkGeographyMetadata(params?: ReadGeographiesParams): Promise<GeographyMetadata[]> {
   const geographies = await readGeographies(params)
   const geography_ids = geographies.map(geography => {
     return `'${geography.geography_id}'`
@@ -107,11 +125,10 @@ export async function isGeographyPublished(geography_id: UUID) {
   if (result.rows.length === 0) {
     throw new NotFoundError(`geography_id ${geography_id} not found`)
   }
-  log.info('is geography published', geography_id, Boolean(result.rows[0].publish_date))
   return Boolean(result.rows[0].publish_date)
 }
 
-export async function editGeography(geography: Geography) {
+export async function editGeography(geography: Geography): Promise<Geography> {
   // validate TODO
   if (await isGeographyPublished(geography.geography_id)) {
     throw new Error('Cannot edit published Geography')
@@ -128,13 +145,16 @@ export async function editGeography(geography: Geography) {
     }
   })
   const sql = `UPDATE ${schema.TABLE.geographies} SET ${conditions} WHERE geography_id='${geography.geography_id}' AND publish_date IS NULL`
+
   await client.query(sql, vals.values())
-  return geography
+  const res = await readSingleGeography(geography.geography_id)
+  return res
 }
 
 export async function deleteGeography(geography_id: UUID) {
+  await readSingleGeography(geography_id)
   if (await isGeographyPublished(geography_id)) {
-    throw new Error('Cannot edit published Geography')
+    throw new AlreadyPublishedError('Cannot delete published Geography')
   }
 
   const client = await getWriteableClient()
@@ -143,8 +163,12 @@ export async function deleteGeography(geography_id: UUID) {
   return geography_id
 }
 
-export async function publishGeography(params: PublishGeographiesParams) {
-  const { geography_id, publish_date } = params
+export async function publishGeography(params: PublishGeographiesParams): Promise<Geography> {
+  /* publish_date is a param instead of a default, because when a Policy is published,
+   * we want to be able to set the publish_date of any associated Geography to be
+   * identical to the publish_date of the Policy.
+   */
+  const { geography_id, publish_date = Date.now() } = params
   try {
     const client = await getWriteableClient()
 
@@ -157,28 +181,35 @@ export async function publishGeography(params: PublishGeographiesParams) {
     const conditions = []
     conditions.push(`publish_date = ${vals.add(publish_date)}`)
     const sql = `UPDATE ${schema.TABLE.geographies} SET ${conditions} where geography_id=${vals.add(geography_id)}`
-
     await client.query(sql, vals.values())
-    return geography_id
+    const res = await readSingleGeography(geography_id)
+    return res
   } catch (err) {
     await log.error(err)
     throw err
   }
 }
 
-export async function writeGeographyMetadata(geography_metadata: GeographyMetadata) {
-  const client = await getWriteableClient()
-  const sql = `INSERT INTO ${schema.TABLE.geography_metadata} (${cols_sql(
-    schema.TABLE_COLUMNS.geography_metadata
-  )}) VALUES (${vals_sql(schema.TABLE_COLUMNS.geography_metadata)}) RETURNING *`
-  const values = vals_list(schema.TABLE_COLUMNS.geography_metadata, {
-    geography_id: geography_metadata.geography_id,
-    geography_metadata: geography_metadata.geography_metadata
-  })
-  const {
-    rows: [recorded_metadata]
-  }: { rows: Recorded<Geography>[] } = await client.query(sql, values)
-  return { ...geography_metadata, ...recorded_metadata }
+export async function writeGeographyMetadata(geography_metadata: GeographyMetadata): Promise<GeographyMetadata> {
+  try {
+    await readSingleGeography(geography_metadata.geography_id)
+    const client = await getWriteableClient()
+    const sql = `INSERT INTO ${schema.TABLE.geography_metadata} (${cols_sql(
+      schema.TABLE_COLUMNS.geography_metadata
+    )}) VALUES (${vals_sql(schema.TABLE_COLUMNS.geography_metadata)}) RETURNING *`
+    const values = vals_list(schema.TABLE_COLUMNS.geography_metadata, {
+      geography_id: geography_metadata.geography_id,
+      geography_metadata: geography_metadata.geography_metadata
+    })
+    const {
+      rows: [recorded_metadata]
+    }: { rows: Recorded<Geography>[] } = await client.query(sql, values)
+    return { ...geography_metadata, ...recorded_metadata }
+  } catch (err) {
+    throw new DependencyMissingError(
+      `metadata not written, because no geography exists for geography_id ${geography_metadata.geography_id}`
+    )
+  }
 }
 
 export async function readSingleGeographyMetadata(geography_id: UUID): Promise<GeographyMetadata> {
@@ -191,7 +222,7 @@ export async function readSingleGeographyMetadata(geography_id: UUID): Promise<G
   return { geography_id, geography_metadata: result.rows[0].geography_metadata }
 }
 
-export async function updateGeographyMetadata(geography_metadata: GeographyMetadata) {
+export async function updateGeographyMetadata(geography_metadata: GeographyMetadata): Promise<GeographyMetadata> {
   await readSingleGeographyMetadata(geography_metadata.geography_id)
   const client = await getWriteableClient()
   const sql = `UPDATE ${schema.TABLE.geography_metadata}
@@ -204,4 +235,17 @@ export async function updateGeographyMetadata(geography_metadata: GeographyMetad
     ...geography_metadata,
     ...recorded_metadata
   }
+}
+
+export async function deleteGeographyMetadata(geography_id: UUID) {
+  const client = await getWriteableClient()
+  try {
+    await readSingleGeographyMetadata(geography_id)
+    const sql = `DELETE FROM ${schema.TABLE.geography_metadata} WHERE geography_id = '${geography_id}'`
+    await client.query(sql)
+  } catch (err) {
+    // nothing to do if we attempted to delete a non-existent metadata
+    await log.error(`deleteGeographyMetadata called on non-existent metadata for ${geography_id}`, err.stack)
+  }
+  return geography_id
 }
