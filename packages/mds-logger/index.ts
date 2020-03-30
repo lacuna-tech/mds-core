10f02844f2af96c17025116afde8a5230f40d871
drtyh20 <michael@ellis-and-associates.com>
2020-03-30 09:10:28 -0400

    Implement new method to redact sensitive text.

--- packages/mds-logger/index.ts
+++ packages/mds-logger/index.ts
@@ -1,5 +1,5 @@
 /*
-    Copyright 2019 City of Los Angeles.
+    Copyright 2019-2020 City of Los Angeles.
 
     Licensed under the Apache License, Version 2.0 (the "License");
     you may not use this file except in compliance with the License.
@@ -14,112 +14,27 @@
     limitations under the License.
  */
 
-// Sorry about all the `any` type declarations, but log messages can be
-// arbitrarily nested JS objects.
+const logger: Pick<Console, 'info' | 'warn' | 'error'> = console
+type LogLevel = keyof typeof logger
 
-/* eslint no-console: "off" */
-
-const { env } = process
-
-type LogLevel = 'INFO' | 'WARN' | 'ERROR'
-
-interface Datum {
-  lat?: string
-  lng?: string
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  [propName: string]: any
-}
-
-function makeCensoredDatum(datum: Datum) {
-  if (datum instanceof Error) {
-    return datum.toString()
-  }
-  const censoredDatum: Datum = { ...datum }
-  if (censoredDatum.lat != null) {
-    censoredDatum.lat = 'CENSORED_LAT'
-  }
-  if (censoredDatum.lng != null) {
-    censoredDatum.lng = 'CENSORED_LNG'
-  }
-  return censoredDatum
-}
-
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-function isAtomic(item: any) {
-  const type = typeof item
-  return ['string', 'number', 'boolean', 'undefined'].includes(type) || item == null
-}
-
-// just in case we have to censor something nested like
-// { data: [{ lat:1, lng:2 }] }
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-function makeCensoredLogMsgRecurse(msg: any): any {
-  if (isAtomic(msg)) {
-    return msg
-  }
-  if (Array.isArray(msg)) {
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    return msg.map((arr_item: any) => makeCensoredLogMsgRecurse(arr_item))
-  }
-  const censoredObject = makeCensoredDatum(msg)
-  if (typeof censoredObject === 'object') {
-    Object.keys(censoredObject).forEach((key: string) => {
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      const val: any = censoredObject[key]
-      if (!isAtomic(val)) {
-        censoredObject[key] = makeCensoredLogMsgRecurse(val)
-      }
-    })
-  }
-  return censoredObject
-}
-
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-function makeCensoredLogMsg(...msgs: any[]): any[] {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  return makeCensoredLogMsgRecurse(msgs).map((item: any) =>
-    // never print out '[object Object]'
-    String(item) === '[object Object]' ? JSON.stringify(item) : item
+const redact = (args: unknown[]): string[] =>
+  args.map(arg =>
+    JSON.stringify(arg instanceof Error ? arg.toString() : arg, (k, v) =>
+      ['lat', 'lng'].includes(k) ? '[REDACTED]' : v
+    )
   )
-}
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-function info(...msg: any[]): any[] {
-  if (env.QUIET) {
-    return []
+const log = (level: LogLevel, ...args: unknown[]): string[] => {
+  if (!process.env.QUIET) {
+    const redacted = redact(args)
+    logger[level](level.toUpperCase(), ...redacted)
+    return redacted
   }
-  const censoredMsg = makeCensoredLogMsg(...msg)
-  console.info('INFO', ...censoredMsg)
-  return censoredMsg
+  return []
 }
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-function warn(...msg: any[]): any[] {
-  if (env.QUIET) {
-    return []
-  }
-  const censoredMsg = makeCensoredLogMsg(...msg)
-  console.warn('WARN', ...censoredMsg)
-  return censoredMsg
-}
-
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-function error(...msg: any[]): any[] {
-  if (env.QUIET) {
-    return []
-  }
-  const censoredMsg = makeCensoredLogMsg(...msg)
-  console.error('ERROR', ...censoredMsg)
-  return censoredMsg
-}
-
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-function log(logLevel: LogLevel, ...msg: any[]): any[] {
-  return {
-    INFO: info,
-    WARN: warn,
-    ERROR: error
-  }[logLevel](...msg)
-}
+const info = (...args: unknown[]) => log('info', ...args)
+const warn = (...args: unknown[]) => log('warn', ...args)
+const error = (...args: unknown[]) => log('error', ...args)
 
 export = { log, info, warn, error }
