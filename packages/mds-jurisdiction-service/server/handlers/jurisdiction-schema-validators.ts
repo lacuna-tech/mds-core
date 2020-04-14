31b05d03dc560d2878bb886cdf8cc92882fe5f93
drtyh20 <michael@ellis-and-associates.com>
2020-04-14 17:57:17 -0400

    Use object schema for jurisdiction validation

--- packages/mds-jurisdiction-service/server/handlers/jurisdiction-schema-validators.ts
+++ packages/mds-jurisdiction-service/server/handlers/jurisdiction-schema-validators.ts
@@ -19,21 +19,24 @@ import {
   stringSchema,
   timestampSchema,
   ValidateSchema,
-  ValidationError
+  ValidationError,
+  SchemaBuilder
 } from '@mds-core/mds-schema-validators'
-import { UUID, Timestamp } from '@mds-core/mds-types'
+import { Timestamp } from '@mds-core/mds-types'
 import { JurisdictionDomainModel } from 'packages/mds-jurisdiction-service/@types'
+
+const jurisdictionSchema = (max: Timestamp = Date.now()) =>
+  SchemaBuilder.object().keys({
+    jurisdiction_id: uuidSchema,
+    agency_key: stringSchema,
+    agency_name: stringSchema,
+    geography_id: uuidSchema,
+    timestamp: timestampSchema.max(max)
+  })
 
 export const ValidateJurisdiction = (jurisdiction: JurisdictionDomainModel): JurisdictionDomainModel => {
   try {
-    ValidateSchema<UUID>(jurisdiction.jurisdiction_id, uuidSchema, { property: 'jurisdiction_id', required: true })
-    ValidateSchema<string>(jurisdiction.agency_key, stringSchema, { property: 'agency_key', required: true })
-    ValidateSchema<string>(jurisdiction.agency_key, stringSchema, { property: 'agency_key', required: true })
-    ValidateSchema<UUID>(jurisdiction.geography_id, uuidSchema, { property: 'geography_id', required: true })
-    ValidateSchema<Timestamp>(jurisdiction.timestamp, timestampSchema.max(Date.now()), {
-      property: 'timestamp',
-      required: true
-    })
+    ValidateSchema<JurisdictionDomainModel>(jurisdiction, jurisdictionSchema(Date.now()))
     return jurisdiction
   } catch (error) {
     throw new ValidationError('Invalid Jurisdiction', { jurisdiction, error })
