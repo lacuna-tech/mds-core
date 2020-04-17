2008955029b9390c2baf30591cf532d63617f507
Jane Huang <jane.x.huang@gmail.com>
2020-04-17 19:19:26 -0400

    mds-policy-author create handler has correct types

--- packages/mds-policy-author/request-handlers.ts
+++ packages/mds-policy-author/request-handlers.ts
@@ -1,9 +1,9 @@
 import logger from '@mds-core/mds-logger'
 import db from '@mds-core/mds-db'
 import { BadParamsError } from '@mds-core/mds-utils'
-import { PolicyAuthorApiRequest, PolicyAuthorApiResponse } from './types'
+import { PolicyAuthorApiRequest, PolicyAuthorGetPoliciesResponse } from './types'
 
-const getPolicies = async (req: PolicyAuthorApiRequest, res: PolicyAuthorApiResponse) => {
+const getPolicies = async (req: PolicyAuthorApiRequest, res: PolicyAuthorGetPoliciesResponse) => {
   const { get_published = null, get_unpublished = null } = req.query
   const params = { get_published, get_unpublished }
   if (get_published) {
@@ -19,16 +19,15 @@ const getPolicies = async (req: PolicyAuthorApiRequest, res: PolicyAuthorApiResp
 
     // Let's not worry about filtering for just active policies at the moment.
     res.status(200).send({ version: res.locals.version, policies })
-  } catch (err) {
-    logger.error('failed to read policies', err)
-    if (err instanceof BadParamsError) {
+  } catch (error) {
+    logger.error('failed to read policies', error)
+    if (error instanceof BadParamsError) {
       res.status(400).send({
-        result:
-          'Cannot set both get_unpublished and get_published to be true. If you want all policies, set both params to false or do not send them.'
+        error
       })
     } else {
       res.status(404).send({
-        result: 'not found'
+        error
       })
     }
   }
--- packages/mds-policy-author/types.ts
+++ packages/mds-policy-author/types.ts
@@ -1,8 +1,12 @@
-import { ApiRequest, ApiResponse } from '@mds-core/mds-api-server'
+import { ApiRequest, ApiVersionedResponse } from '@mds-core/mds-api-server'
+import { Policy } from '@mds-core/mds-types'
 
 export const POLICY_AUTHOR_API_SUPPORTED_VERSIONS = ['0.1.0'] as const
 export type POLICY_AUTHOR_API_SUPPORTED_VERSION = typeof POLICY_AUTHOR_API_SUPPORTED_VERSIONS[number]
 export const [POLICY_AUTHOR_API_DEFAULT_VERSION] = POLICY_AUTHOR_API_SUPPORTED_VERSIONS
 
 export type PolicyAuthorApiRequest = ApiRequest
-export type PolicyAuthorApiResponse = ApiResponse
+
+type PolicyAuthorApiResponse<TBody = {}> = ApiVersionedResponse<POLICY_AUTHOR_API_SUPPORTED_VERSION, TBody>
+
+export type PolicyAuthorGetPoliciesResponse = PolicyAuthorApiResponse<{ policies: Policy[] }>
