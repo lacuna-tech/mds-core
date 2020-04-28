aa06ddfa1e90f618ba18cd967b38c8413db5e7fe
Neil Goldader <neilgoldader@gmail.com>
2020-04-28 12:21:51 -0400

    Refactor mds-web-sockets container image to launch from server config in package

--- container-images/mds-web-sockets/webpack.config.ts
+++ container-images/mds-web-sockets/webpack.config.ts
@@ -1,3 +1,3 @@
 import webpack from '@mds-core/mds-webpack-config'
 
-export default webpack.Bundle('./index.ts').UsingDefaultConfig()
+export default webpack.Bundle('../../packages/mds-web-sockets/launch_server.ts').UsingDefaultConfig()
