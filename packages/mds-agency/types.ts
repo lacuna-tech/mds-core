16446edcf74d36b74419f92976330a87e0b395e7
Jane Huang <jane.x.huang@gmail.com>
2020-05-18 14:50:30 -0400

    Removing versiono param from agency payload to conform with general versioninig policy.

--- packages/mds-agency/types.ts
+++ packages/mds-agency/types.ts
@@ -1,6 +1,6 @@
 import { UUID, Device, VehicleEvent, Telemetry, Timestamp, Recorded, VEHICLE_STATUS, Stop } from '@mds-core/mds-types'
 import { MultiPolygon } from 'geojson'
-import { ApiRequest, ApiVersionedResponse, ApiClaims } from '@mds-core/mds-api-server'
+import { ApiRequest, ApiClaims, ApiResponse } from '@mds-core/mds-api-server'
 import { Params, ParamsDictionary } from 'express-serve-static-core'
 
 export const AGENCY_API_SUPPORTED_VERSIONS = ['0.4.1'] as const
@@ -11,8 +11,7 @@ export type AgencyApiRequest<P extends Params = ParamsDictionary> = ApiRequest<P
 
 export type AgencyApiAccessTokenScopes = 'admin:all' | 'vehicles:read'
 
-export type AgencyApiResponse<TBody = any> = ApiVersionedResponse<
-  AGENCY_API_SUPPORTED_VERSION,
+export type AgencyApiResponse<TBody = any> = ApiResponse<
   ApiClaims<AgencyApiAccessTokenScopes> & {
     provider_id: UUID
   },
