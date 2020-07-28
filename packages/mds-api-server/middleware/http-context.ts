2bda0ba679704a56ef30f0254386bc389e5f43c6
Neil Goldader <neilgoldader@gmail.com>
2020-07-28 13:34:30 -0400

    Remove plurality

--- packages/mds-api-server/middleware/http-context.ts
+++ packages/mds-api-server/middleware/http-context.ts
@@ -2,8 +2,6 @@ import httpContext from 'express-http-context'
 import express from 'express'
 import { ApiRequest, ApiResponse } from '../@types'
 
-const HttpContextMiddleware = httpContext.middleware
-
 /**
  * Middleware that extracts the X-Request-Id header set by the gateway,
  * and includes it in contextual logs for a given request.
@@ -13,6 +11,6 @@ const RequestIdMiddleware = (req: ApiRequest, res: ApiResponse, next: express.Ne
   return next()
 }
 
-export const HttpContextMiddlewares = () => {
-  return [HttpContextMiddleware, RequestIdMiddleware]
+export const HttpContextMiddleware = () => {
+  return [httpContext.middleware, RequestIdMiddleware]
 }
