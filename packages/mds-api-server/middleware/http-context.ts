62b75773a0c81e0545cb48d5b7e1a1162f552205
Neil Goldader <neilgoldader@gmail.com>
2020-07-28 10:30:27 -0400

    Pull header from req, not res

--- packages/mds-api-server/middleware/http-context.ts
+++ packages/mds-api-server/middleware/http-context.ts
@@ -5,6 +5,6 @@ import { ApiRequest, ApiResponse } from '../@types'
 export const HttpContextMiddleware = () => httpContext.middleware
 
 export const RequestIdMiddleware = () => (req: ApiRequest, res: ApiResponse, next: express.NextFunction) => {
-  httpContext.set('x-request-id', res.get('x-request-id'))
+  httpContext.set('x-request-id', req.get('x-request-id'))
   return next()
 }
