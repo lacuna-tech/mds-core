b0b1295d86c56c1463ac5e844ee13f835781048d
Jane Huang <jane.x.huang@gmail.com>
2020-08-10 20:05:02 -0400

    Types for vehicle event transformers should refer to version-specific types, not the vanilla type.

---
+++ packages/mds-types/transformers/0_4_1_to_1_0_0/devices.ts
@@ -0,0 +1,6 @@
+import { Device_v1_0_0 } from '../../index'
+import { Device_v0_4_1 } from '../@types'
+
+export function convert_v0_4_1_device_to_1_0_0(device: Device_v0_4_1): Device_v1_0_0 | null {
+  return null
+}
