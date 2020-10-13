c1e3e3cdbf34240afd9548864db83cd1e991726a
drtyh20 <michael.durling@lacuna.ai>
2020-10-13 08:45:37 -0400

    Update mds-jurisdiction-service to latest pattern

--- packages/mds-jurisdiction-service/service/provider.ts
+++ packages/mds-jurisdiction-service/service/provider.ts
@@ -14,13 +14,109 @@
     limitations under the License.
  */
 
-import { ServiceProvider, ProcessController } from '@mds-core/mds-service-helpers'
-import { JurisdictionRepository } from './repository'
-import { JurisdictionService } from '../@types'
-import * as handlers from './handlers'
+import logger from '@mds-core/mds-logger'
+import {
+  ServiceProvider,
+  ProcessController,
+  ServiceResult,
+  ServiceResponse,
+  ServiceException
+} from '@mds-core/mds-service-helpers'
+import { Nullable } from '@mds-core/mds-types'
+import { JurisdictionRepository } from '../repository'
+import {
+  CreateJurisdictionDomainModel,
+  GetJurisdictionsOptions,
+  JurisdictionDomainModel,
+  JurisdictionIdType,
+  JurisdictionService,
+  UpdateJurisdictionDomainModel
+} from '../@types'
+import { ValidateJurisdictionForCreate } from './validators'
 
 export const JurisdictionServiceProvider: ServiceProvider<JurisdictionService> & ProcessController = {
   start: JurisdictionRepository.initialize,
   stop: JurisdictionRepository.shutdown,
-  ...handlers
+
+  createJurisdiction: async (
+    model: CreateJurisdictionDomainModel
+  ): Promise<ServiceResponse<JurisdictionDomainModel>> => {
+    try {
+      const [jurisdiction] = await JurisdictionRepository.createJurisdictions(
+        [model].map(ValidateJurisdictionForCreate)
+      )
+      return ServiceResult(jurisdiction)
+    } catch (error) /* istanbul ignore next */ {
+      const exception = ServiceException('Error Creating Jurisdiction', error)
+      logger.error(exception, error)
+      return exception
+    }
+  },
+
+  createJurisdictions: async (
+    models: CreateJurisdictionDomainModel[]
+  ): Promise<ServiceResponse<JurisdictionDomainModel[]>> => {
+    try {
+      const jurisdictions = await JurisdictionRepository.createJurisdictions(models.map(ValidateJurisdictionForCreate))
+      return ServiceResult(jurisdictions)
+    } catch (error) /* istanbul ignore next */ {
+      const exception = ServiceException('Error Creating Jurisdictions', error)
+      logger.error(exception, error)
+      return exception
+    }
+  },
+
+  deleteJurisdiction: async (
+    jurisdiction_id: JurisdictionIdType
+  ): Promise<ServiceResponse<Pick<JurisdictionDomainModel, 'jurisdiction_id'>>> => {
+    try {
+      const deleted = await JurisdictionRepository.deleteJurisdiction(jurisdiction_id)
+      return ServiceResult(deleted)
+    } catch (error) /* istanbul ignore next */ {
+      const exception = ServiceException('Error Deleting Jurisdiction', error)
+      logger.error(exception, error)
+      return exception
+    }
+  },
+
+  getJurisdiction: async (
+    jurisdiction_id: JurisdictionIdType,
+    options?: Nullable<GetJurisdictionsOptions>
+  ): Promise<ServiceResponse<JurisdictionDomainModel>> => {
+    try {
+      const jurisdiction = await JurisdictionRepository.readJurisdiction(jurisdiction_id, options ?? {})
+      return ServiceResult(jurisdiction)
+    } catch (error) /* istanbul ignore next */ {
+      const exception = ServiceException('Error Reading Jurisdiction', error)
+      logger.error(exception, error)
+      return exception
+    }
+  },
+
+  getJurisdictions: async (
+    options?: Nullable<GetJurisdictionsOptions>
+  ): Promise<ServiceResponse<JurisdictionDomainModel[]>> => {
+    try {
+      const jurisdicitons = await JurisdictionRepository.readJurisdictions(options ?? {})
+      return ServiceResult(jurisdicitons)
+    } catch (error) /* istanbul ignore next */ {
+      const exception = ServiceException('Error Reading Jurisdictions', error)
+      logger.error(exception, error)
+      return exception
+    }
+  },
+
+  updateJurisdiction: async (
+    jurisdiction_id: JurisdictionIdType,
+    jurisdiction: UpdateJurisdictionDomainModel
+  ): Promise<ServiceResponse<JurisdictionDomainModel>> => {
+    try {
+      const updated = await JurisdictionRepository.updateJurisdiction(jurisdiction_id, jurisdiction)
+      return ServiceResult(updated)
+    } catch (error) /* istanbul ignore next */ {
+      const exception = ServiceException('Error Updating Jurisdiction', error)
+      logger.error(exception, error)
+      return exception
+    }
+  }
 }
