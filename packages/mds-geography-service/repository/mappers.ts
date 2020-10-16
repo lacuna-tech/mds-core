fe2bcbecf4ff4ddf7d718f0d42c3f7f763141477
drtyh20 <michael.durling@lacuna.ai>
2020-10-16 06:58:10 -0400

    Create metadata model mapper

--- packages/mds-geography-service/repository/mappers.ts
+++ packages/mds-geography-service/repository/mappers.ts
@@ -1,7 +1,13 @@
 import { Timestamp } from '@mds-core/mds-types'
 import { IdentityColumn, ModelMapper } from '@mds-core/mds-repository'
 import { GeographyEntityModel } from './entities/geography-entity'
-import { GeographyDomainCreateModel, GeographyDomainModel } from '../@types'
+import {
+  GeographyDomainCreateModel,
+  GeographyDomainModel,
+  GeographyMetadataDomainModel,
+  GeographyMetadataDomainCreateModel
+} from '../@types'
+import { GeographyMetadataEntityModel } from './entities/geography-metadata-entity'
 
 type GeographyEntityToDomainOptions = Partial<{}>
 
@@ -40,3 +46,28 @@ export const GeographyDomainToEntityCreate = ModelMapper<
     return { name, description, effective_date, publish_date, prev_geographies, ...domain }
   }
 )
+
+type GeographyMetadataEntityToDomainOptions = Partial<{}>
+
+export const GeographyMetadataEntityToDomain = ModelMapper<
+  GeographyMetadataEntityModel,
+  GeographyMetadataDomainModel,
+  GeographyMetadataEntityToDomainOptions
+>((entity, options) => {
+  const { id, geography_metadata = null, ...domain } = entity
+  return { geography_metadata, ...domain }
+})
+
+type GeographyMetadataEntityCreateOptions = Partial<{
+  recorded: Timestamp
+}>
+
+export type GeographyMetadataEntityCreateModel = Omit<GeographyMetadataEntityModel, keyof IdentityColumn>
+
+export const GeographyMetadataDomainToEntityCreate = ModelMapper<
+  GeographyMetadataDomainCreateModel,
+  GeographyMetadataEntityCreateModel,
+  GeographyMetadataEntityCreateOptions
+>(({ geography_metadata = null, ...domain }, options) => {
+  return { geography_metadata, ...domain }
+})
