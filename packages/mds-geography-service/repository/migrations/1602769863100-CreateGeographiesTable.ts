baf0a09bcec00ea28e470649e41afa00eecb54be
drtyh20 <michael.durling@lacuna.ai>
2020-10-16 09:03:14 -0400

    Fix index creation for legacy migrations

--- packages/mds-geography-service/repository/migrations/1602769863100-CreateGeographiesTable.ts
+++ packages/mds-geography-service/repository/migrations/1602769863100-CreateGeographiesTable.ts
@@ -12,11 +12,11 @@ export class CreateGeographiesTable1602769863100 implements MigrationInterface {
         `CREATE TABLE "geographies" ("id" bigint GENERATED ALWAYS AS IDENTITY, "geography_id" uuid NOT NULL, "name" character varying(255), "description" character varying(255), "effective_date" bigint, "publish_date" bigint, "prev_geographies" uuid array, "geography_json" json NOT NULL, CONSTRAINT "geographies_pkey" PRIMARY KEY ("geography_id"))`
       )
       await queryRunner.query(`CREATE UNIQUE INDEX "idx_id_geographies" ON "geographies" ("id") `)
-      await queryRunner.query(`CREATE INDEX "idx_publish_date_geographies" ON "geographies" ("publish_date") `)
     } else {
       // Legacy migration code created an index that doesn't follow the standard naming convention
       await queryRunner.query(`DROP INDEX IF EXISTS geographies_publish_date_idx`)
     }
+    await queryRunner.query(`CREATE INDEX "idx_publish_date_geographies" ON "geographies" ("publish_date") `)
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
