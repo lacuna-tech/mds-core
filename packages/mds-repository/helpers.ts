e697b58bff8161e9847b44c7100b78a5eb222f89
drtyh20 <michael.durling@lacuna.ai>
2020-11-03 15:47:20 -0500

    Fix tableExists/tableNotExists checks

--- packages/mds-repository/helpers.ts
+++ packages/mds-repository/helpers.ts
@@ -1,15 +1,15 @@
 import { QueryRunner } from 'typeorm'
 
 const selectTableNameFromSchema = (name: string) =>
-  `SELECT "table_name" FROM "information_schema"."tables" WHERE "table_schema" = current_schema() AND "table_name" = '${name}'`
+  `SELECT COUNT(*) FROM "information_schema"."tables" WHERE "table_schema" = current_schema() AND "table_name" = '${name}'`
 
 export const MigrationHelper = (queryRunner: QueryRunner) => ({
   tableExists: async (name: string) => {
-    const [table_name] = await queryRunner.query(selectTableNameFromSchema(name))
-    return table_name === name
+    const [{ count }]: [{ count: number }] = await queryRunner.query(selectTableNameFromSchema(name))
+    return count !== 0
   },
   tableNotExists: async (name: string) => {
-    const [table_name] = await queryRunner.query(selectTableNameFromSchema(name))
-    return table_name === undefined
+    const [{ count }]: [{ count: number }] = await queryRunner.query(selectTableNameFromSchema(name))
+    return count === 0
   }
 })
