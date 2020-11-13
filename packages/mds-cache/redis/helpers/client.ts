6b875a7d3131546a8453263c8d8b9866643759a3
Mark Maxham <mark.maxham@gmail.com>
2020-11-13 13:06:19 -0800

    make sure that initial connection failure doesn't totally hork anything that uses the cache because istio can fail to start the network stack ahead of bringing the service up

--- packages/mds-cache/redis/helpers/client.ts
+++ packages/mds-cache/redis/helpers/client.ts
@@ -1,5 +1,6 @@
 import Redis from 'ioredis'
 import { cleanEnv, host, num } from 'envalid'
+import logger from '@mds-core/mds-logger'
 
 const { REDIS_PORT, REDIS_HOST, REDIS_PASS } = cleanEnv(process.env, {
   REDIS_PORT: num({ default: 6379 }),
@@ -7,9 +8,24 @@ const { REDIS_PORT, REDIS_HOST, REDIS_PASS } = cleanEnv(process.env, {
 })
 
 export const initClient = async () => {
-  const client = new Redis({ lazyConnect: true, port: REDIS_PORT, host: REDIS_HOST, password: REDIS_PASS })
-
-  await client.connect()
-
+  const client = new Redis({
+    lazyConnect: true,
+    maxRetriesPerRequest: 1000, // 20 is default, but that may not be long enough
+    port: REDIS_PORT,
+    host: REDIS_HOST,
+    password: REDIS_PASS
+  })
+  client.on('connect', () => {
+    logger.warn('connected, yay')
+  })
+  try {
+    // try to connect; if fails initially, that's okay; we don't want to throw,
+    // we want it to keep using its retry mechanism
+    await client.connect()
+  } catch (err) {
+    // suppress initial connection failure
+    logger.error('failed initial connect to redis, will keep trying')
+    // log and keep going
+  }
   return client
 }
