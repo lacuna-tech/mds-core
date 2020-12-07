6f032ecfb643bcfe1179e2f6314b190424a3fc2e
Mark Maxham <mark.maxham@gmail.com>
2020-12-07 12:25:20 -0800

    moar tests

--- packages/mds-transactions-service/tests/service.spec.ts
+++ packages/mds-transactions-service/tests/service.spec.ts
@@ -27,12 +27,16 @@ const provider_id = '3452fa87-bfd7-42c5-9c53-5e07bde13671'
 const transaction_id = '37bd96ac-69bd-4634-9b22-ff081d7a5a09'
 const receipt_id = 'a5eb612e-a154-4339-a760-aee95908dc51'
 
+const receipt = { receipt_id, timestamp: Date.now(), receipt_details: {}, origin_url: '' }
+
+const malformed_uuid = '176b8453-ccaf-41c7-a4df-f7b3f80bddd1xxxxxxx'
+
 describe('Transaction Service Tests', () => {
   beforeAll(async () => {
     await TransactionServer.start()
   })
 
-  it('Post Transaction', async () => {
+  it('Post Good Transaction', async () => {
     const transaction = await TransactionServiceClient.createTransaction({
       transaction_id,
       provider_id,
@@ -40,10 +44,27 @@ describe('Transaction Service Tests', () => {
       timestamp: Date.now(),
       amount: 100, // "I'd buy THAT for a dollar!"
       fee_type: 'base_fee',
-      receipt: { receipt_id, timestamp: Date.now(), receipt_details: {}, origin_url: '' }
+      receipt
     })
     expect(transaction.device_id).toEqual(device_id)
     expect(transaction.transaction_id).toEqual(transaction_id)
+  })
+
+  it('Post Transaction with malformed transaction_id', async () => {
+    try {
+      await TransactionServiceClient.createTransaction({
+        transaction_id: malformed_uuid,
+        provider_id,
+        device_id,
+        timestamp: Date.now(),
+        amount: 100, // "I'd buy THAT for a dollar!"
+        fee_type: 'base_fee',
+        receipt
+      })
+      expect('did not happen').toBe('happened')
+    } catch (err) {
+      expect(err.type).toBe('ValidationError')
+    }
   })
 
   it('Get All Transactions', async () => {
@@ -57,6 +78,24 @@ describe('Transaction Service Tests', () => {
     const transaction = await TransactionServiceClient.getTransaction(transaction_id)
     expect(transaction.transaction_id).toEqual(transaction_id)
   })
+
+  // post dup trans id
+  // post trans with missing fields
+  // post trans with non-UUID
+
+  // operations
+  // post dup op id
+  // post op with missing fields
+  // post op with bad op
+  // post op with non-UUID
+  // post op on non-existant transaction id
+
+  // status
+  // post dup stat id
+  // post stat with missing fields
+  // post stat with non-UUID
+  // post stat with bad stat
+  // post stat on non-existant transaction id
 
   afterAll(async () => {
     await TransactionServer.stop()
