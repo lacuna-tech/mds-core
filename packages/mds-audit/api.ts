4d2c6ffc86181009dd24f30b985a22816151c607
Neil Goldader <neilgoldader@gmail.com>
2021-01-06 13:35:06 -0500

    Re-add code which was removed for testing

--- packages/mds-audit/api.ts
+++ packages/mds-audit/api.ts
@@ -742,7 +742,10 @@ function api(app: express.Express): express.Express {
     pathPrefix('/trips/:audit_trip_id/attach/:mimetype'),
     multipartFormUpload,
     async (req, res: PostAuditAttachmentResponse) => {
-      const { audit_trip_id } = res.locals
+      const { audit, audit_trip_id } = res.locals
+
+      if (!audit) return res.status(404).send({ error: new NotFoundError('audit not found', { audit_trip_id }) })
+
       try {
         const attachment = await writeAttachment(req.file, audit_trip_id)
         res.status(200).send({
