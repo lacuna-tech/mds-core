bcddc9bb7e2b4ec653231ec19ad73e422cc95ec5
Neil Goldader <neilgoldader@gmail.com>
2021-05-04 19:50:26 -0400

    Add transaction-api spec, move spec & schema generation to mds-transaction-api package

--- packages/mds-transaction-api/generate-schemas.ts
+++ packages/mds-transaction-api/generate-schemas.ts
@@ -1,4 +1,4 @@
-import { schemas } from './service/validators'
+import { schemas } from '@mds-core/mds-transaction-service'
 import { writeFileSync } from 'fs'
 
 schemas.map(({ name, schema }) => writeFileSync(`schema-gen/${name}.json`, JSON.stringify(schema, null, 2)))
