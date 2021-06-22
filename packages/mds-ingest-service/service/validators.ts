1735a2a0a56877039092e4d947c3717bfc1a7960
Neil Goldader <neilgoldader@gmail.com>
2021-06-22 19:53:58 -0400

    Switch device validation to AJV

--- packages/mds-ingest-service/service/validators.ts
+++ packages/mds-ingest-service/service/validators.ts
@@ -15,8 +15,15 @@
  */
 
 import Joi from 'joi'
-import { schemaValidator } from '@mds-core/mds-schema-validators'
-import { VEHICLE_TYPES, PROPULSION_TYPES, VEHICLE_EVENTS, VEHICLE_STATES } from '@mds-core/mds-types'
+import { schemaValidator, SchemaValidator } from '@mds-core/mds-schema-validators'
+import {
+  VEHICLE_TYPES,
+  PROPULSION_TYPES,
+  VEHICLE_EVENTS,
+  VEHICLE_STATES,
+  MODALITIES,
+  ACCESSIBILITY_OPTIONS
+} from '@mds-core/mds-types'
 import {
   DeviceDomainModel,
   EventDomainModel,
@@ -24,27 +31,41 @@ import {
   GROUPING_TYPES,
   GetVehicleEventsFilterParams
 } from '../@types'
-import { SchemaValidator } from '@mds-core/mds-schema-validators'
 
-export const {
-  validate: validateDeviceDomainModel,
-  isValid: isValidDeviceDomainModel
-} = schemaValidator<DeviceDomainModel>(
-  Joi.object<DeviceDomainModel>()
-    .keys({
-      device_id: Joi.string().uuid().required(),
-      provider_id: Joi.string().uuid().required(),
-      vehicle_id: Joi.string().required(),
-      vehicle_type: Joi.string()
-        .valid(...Object.keys(VEHICLE_TYPES))
-        .required(),
-      propulsion_types: Joi.array().valid(Joi.string().valid(...Object.keys(PROPULSION_TYPES))),
-      year: Joi.number().allow(null),
-      mfgr: Joi.string().allow(null),
-      model: Joi.string().allow(null)
-    })
-    .unknown(false)
-)
+const uuidSchema = { type: 'string', format: 'uuid' }
+
+export const { validate: validateDeviceDomainModel, $schema: DeviceSchema } = SchemaValidator<DeviceDomainModel>({
+  $id: 'Device',
+  type: 'object',
+  properties: {
+    device_id: uuidSchema,
+    provider_id: uuidSchema,
+    vehicle_id: {
+      type: 'string'
+    },
+    vehicle_type: { type: 'string', enum: VEHICLE_TYPES },
+    propulsion_types: {
+      type: 'array',
+      items: {
+        type: 'string',
+        enum: PROPULSION_TYPES
+      }
+    },
+    accessibility_options: {
+      type: 'array',
+      items: {
+        type: 'string',
+        enum: ACCESSIBILITY_OPTIONS
+      },
+      default: []
+    },
+    modality: { type: 'string', enum: MODALITIES, default: 'micromobility' },
+    year: { type: 'integer' },
+    mfgr: { type: 'string' },
+    model: { type: 'string' }
+  },
+  required: ['device_id', 'provider_id', 'vehicle_id', 'vehicle_type', 'propulsion_types']
+})
 
 /* Separate so we can re-use in the event domain model validator */
 const telemetrySchema = Joi.object<TelemetryDomainModel>()
@@ -64,31 +85,27 @@ const telemetrySchema = Joi.object<TelemetryDomainModel>()
   })
   .unknown(false)
 
-export const {
-  validate: validateTelemetryDomainModel,
-  isValid: isValidTelemetryDomainModel
-} = schemaValidator<DeviceDomainModel>(telemetrySchema)
+export const { validate: validateTelemetryDomainModel, isValid: isValidTelemetryDomainModel } =
+  schemaValidator<DeviceDomainModel>(telemetrySchema)
 
-export const {
-  validate: validateEventDomainModel,
-  isValid: isValidEventDomainModel
-} = schemaValidator<DeviceDomainModel>(
-  Joi.object<EventDomainModel>()
-    .keys({
-      device_id: Joi.string().uuid().required(),
-      provider_id: Joi.string().uuid().required(),
-      timestamp: Joi.number().required(),
-      event_types: Joi.array()
-        .valid(Joi.string().valid(...VEHICLE_EVENTS))
-        .required(),
-      vehicle_state: Joi.string().valid(...VEHICLE_STATES),
-      telemetry_timestamp: Joi.number().allow(null),
-      telemetry: telemetrySchema.allow(null),
-      trip_id: Joi.string().uuid().allow(null),
-      service_area_id: Joi.string().uuid().allow(null)
-    })
-    .unknown(false)
-)
+export const { validate: validateEventDomainModel, isValid: isValidEventDomainModel } =
+  schemaValidator<DeviceDomainModel>(
+    Joi.object<EventDomainModel>()
+      .keys({
+        device_id: Joi.string().uuid().required(),
+        provider_id: Joi.string().uuid().required(),
+        timestamp: Joi.number().required(),
+        event_types: Joi.array()
+          .valid(Joi.string().valid(...VEHICLE_EVENTS))
+          .required(),
+        vehicle_state: Joi.string().valid(...VEHICLE_STATES),
+        telemetry_timestamp: Joi.number().allow(null),
+        telemetry: telemetrySchema.allow(null),
+        trip_id: Joi.string().uuid().allow(null),
+        service_area_id: Joi.string().uuid().allow(null)
+      })
+      .unknown(false)
+  )
 
 export const { validate: validateGetVehicleEventsFilterParams } = SchemaValidator<GetVehicleEventsFilterParams>({
   type: 'object',
